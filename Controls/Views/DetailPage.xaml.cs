a4aaaffc52f4d055d002832b7a60074e5a734431
Joshua Schipull <joshschipull@gmail.com>
2018-04-28 10:42:59 -0500

    Add project files.

---
+++ Controls/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Controls.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Controls.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
